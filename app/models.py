9a83e287ae9f4f4c802a8de5b33adf21dabd2b4b
Cosmin Traistaru <cosmin@traista.ru>
2014-11-27 18:05:08 +0200

    created database class models

--- app/models.py
+++ app/models.py
@@ -1,6 +1,7 @@
 import database
 from app import db
 
+
 class ProdusOld():
     def __init__(self):
         pass
@@ -22,4 +23,49 @@ class ProdusOld():
             return preturi, nume, link, image
 
 
+class Categorie(db.Model):
+    __tablename__ = 'categorie'
+    idCategorie = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    NumeCategorie = db.Column(db.text)
 
+
+class Magazin(db.Model):
+    __tablename__ = 'magazin'
+    idMagazin = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    Nume = db.Column(db.text)
+    LinkMagazin = db.Column(db.text)
+    Descriere = db.Column(db.text)
+
+    def __repr__(self):
+        return "Magazin: {magazin} ".format(magazin=self.Nume)
+
+
+class Pret(db.Model):
+    __tablename__ = 'pret'
+    idPret = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    idProdus = db.Column(db.Integer, db.ForeignKey('produs.idProdus'))
+    NumeProdus = db.relationship('Produs', backref=db.backref('pret',
+                                                              lazy='dynamic'))
+    Pret = db.Column(db.DECIMAL(10, 4))
+    Data = db.Column(db.Date)
+
+
+class Produs(db.Model):
+    __tablename__ = 'produs'
+    idProdus = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    idCategorie = db.Column(db.Integer, db.ForeignKey('categorie.idCategorie'),
+                            default=0)
+    NumeCategorie = db.relationship('Categorie',
+                                    backref=db.backref('produs',
+                                                       lazy='dynamic'))
+    idMagazin = db.Column(db.Integer, db.ForeignKey('categorie.idMagazin'))
+    Nume = db.relationship('Magazin', backref=db.backref('produs',
+                                                         lazy='dynamic'))
+    NumeProdus = db.Column(db.text)
+    LinkProdus = db.Column(db.text)
+    PozaProdus = db.Column(db.text)
+    hash = db.Column(db.VARCHAR(40))
+
+    def __repr__(self):
+        return "Produs: {nume_produs} \nLink Produs: {link_produs}".\
+            format(nume_produs=self.NumeProdus, link_produs=self.LinkProdus)
